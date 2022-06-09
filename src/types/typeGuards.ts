d71cc46cdb9a6763388ad55111d817165ec3cf3c
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-06-10 00:03:32 +0530

    feat: added isAxioserror typeguard

--- src/types/typeGuards.ts
+++ src/types/typeGuards.ts
@@ -1,6 +1,21 @@
+import { AxiosError } from "axios";
 import { Auth } from "./types";
 
 export const isJSError = (e: any): e is Error => e instanceof Error;
+
+export const isAxiosError = (e: any): e is AxiosError => {
+  try {
+    if (
+      (e as AxiosError).response?.status !== undefined &&
+      (e as AxiosError).name === "AxiosError"
+    )
+      return true;
+    else return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
 
 export const isLoginData = (param: any): param is Auth.LoginData => {
   if (param.token && param.user) return true;
