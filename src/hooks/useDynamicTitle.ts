1d94f4d48376baad6124ea47add6c21fe17a6846
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-06-21 17:39:32 +0530

    fix: signin page title

--- src/hooks/useDynamicTitle.ts
+++ src/hooks/useDynamicTitle.ts
@@ -1,10 +1,10 @@
 import { useLocation } from "react-router-dom";
-import { LocalRoutes } from "../constants";
 
 const useDynamicTitle = () => {
   const { pathname } = useLocation();
   const currPath = pathname.split("/")[1];
-  if (currPath !== LocalRoutes.SIGNIN) {
+
+  if (pathname !== "/") {
     document.title = `SG | ${currPath.split("-").join(" ").toUpperCase()}`;
   } else document.title = "SG | SIGNIN";
   return { pathname };
